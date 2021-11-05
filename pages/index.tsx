b1973d8f210be90b35c32768c88d8f35e8b33333
jonsoku2 <whdtjr2792@gmail.com>
2021-11-05 10:38:36 +0900

    feat: configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+interface HomePageProps {}
+
+const Home: NextPage<HomePageProps> = () => {
   return (
     <div className={styles.container}>
       <Head>
